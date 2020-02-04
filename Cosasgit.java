36b92642910d35c0c6ba82abe697a7a587f6e327
noeliaruiz <ruiz@noelia.com>
2020-02-04 18:32:08 +0100

    hacemos una actualizacion

--- Cosasgit.java
+++ Cosasgit.java
@@ -15,7 +15,8 @@ public class Cosasgit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola");
+        System.out.println("Hola, esto es git");
+        System.out.println("Esto es nuevo");
     }
     
 }
